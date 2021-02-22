0de3c6bde2a26177a114e8da12f2fcd34fd50dd8
Dabasan <43123127+Dabasan@users.noreply.github.com>
2021-02-22 11:46:51 +0900

    Update

--- deploy.go
+++ deploy.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 
 	"github.com/otiai10/copy"
+	"github.com/urfave/cli/v2"
 )
 
-func anyFileExists(dir string) (bool, error) {
-	files, err := ioutil.ReadDir(dir)
+func anyFileExists(targetDir string) (bool, error) {
+	files, err := ioutil.ReadDir(targetDir)
 	if err != nil {
 		return false, err
 	}
@@ -23,31 +23,33 @@ func anyFileExists(dir string) (bool, error) {
 	return true, nil
 }
 
-func enumerateDirectories(dir string) ([]string, error) {
-	files, err := ioutil.ReadDir(dir)
+func enumerateDirectories(targetRootDir string) ([]string, error) {
+	files, err := ioutil.ReadDir(targetRootDir)
 	if err != nil {
 		return nil, err
 	}
 
 	ret := make([]string, 0)
 	for _, file := range files {
-		addondir := filepath.Join(dir, file.Name())
-		ret = append(ret, addondir)
+		if file.IsDir() {
+			addonDir := filepath.Join(targetRootDir, file.Name())
+			ret = append(ret, addonDir)
+		}
 	}
 
 	return ret, nil
 }
 
-func deployAddon(srcDir string, dstDir string) error {
-	files, err := ioutil.ReadDir(srcDir)
+func deployAddon(inputRootDir string, outputRootDir string) error {
+	subdirs, err := ioutil.ReadDir(inputRootDir)
 	if err != nil {
 		return err
 	}
 
-	for _, file := range files {
+	for _, subdir := range subdirs {
 		err := copy.Copy(
-			filepath.Join(srcDir, file.Name()),
-			filepath.Join(dstDir, file.Name()))
+			filepath.Join(inputRootDir, subdir.Name()),
+			filepath.Join(outputRootDir, subdir.Name()))
 		if err != nil {
 			return err
 		}
@@ -57,39 +59,82 @@ func deployAddon(srcDir string, dstDir string) error {
 }
 
 func main() {
-	flag.Parse()
-	numFlags := len(flag.Args())
-	if numFlags != 2 {
-		fmt.Fprintln(os.Stderr, "エラー: コマンドライン引数の数が不正です。")
+	var inputRootDir string
+	var outputRootDir string
+	var bHelp bool
+	var bVersion bool
+	app := &cli.App{
+		Name:    "Addon Deployer",
+		Version: "v1.0.0",
+
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "inputRootDir",
+				Aliases:     []string{"i"},
+				Usage:       "Input root directory",
+				Destination: &inputRootDir,
+			},
+			&cli.StringFlag{
+				Name:        "outputRootDir",
+				Aliases:     []string{"o"},
+				Usage:       "Output root directory",
+				Destination: &outputRootDir,
+			},
+		},
+	}
+	cli.HelpFlag = &cli.BoolFlag{
+		Name:        "help",
+		Aliases:     []string{"h"},
+		Usage:       "Displays help",
+		Destination: &bHelp,
+	}
+	cli.VersionFlag = &cli.BoolFlag{
+		Name:        "version",
+		Aliases:     []string{"v"},
+		Usage:       "Displays version",
+		Destination: &bVersion,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
+
+	if bHelp || bVersion {
 		return
 	}
 
-	srcDir := flag.Arg(0)
-	dstDir := flag.Arg(1)
+	if inputRootDir == "" {
+		fmt.Println("エラー: 入力ディレクトリを指定してください")
+		return
+	}
+	if outputRootDir == "" {
+		fmt.Println("エラー: 出力ディレクトリを指定してください")
+		return
+	}
 
-	//すでにdstDirが示すディレクトリ内に何かファイルが存在する場合にはプログラムを終了する。
-	fileExists, err := anyFileExists(dstDir)
+	//すでにoutputRootDirが示すディレクトリ内に何かファイルが存在する場合にはプログラムを終了する
+	fileExists, err := anyFileExists(outputRootDir)
 	if err != nil {
 		panic(err)
 	}
-	if fileExists == true {
-		errMessage := fmt.Sprintf("エラー: %v にはすでにファイルが存在します。", dstDir)
+	if fileExists {
+		errMessage := fmt.Sprintf("エラー: %v にはすでにファイルが存在します", outputRootDir)
 		fmt.Fprintln(os.Stderr, errMessage)
 		return
 	}
 
-	dirs, err := enumerateDirectories(srcDir)
+	subdirs, err := enumerateDirectories(inputRootDir)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, dir := range dirs {
-		err := deployAddon(filepath.Join(dir, "addon"), dstDir)
+	for _, subdir := range subdirs {
+		err := deployAddon(filepath.Join(subdir, "addon"), outputRootDir)
 		if err != nil {
-			fmt.Printf("処理をスキップします。 %v\n", dir)
+			fmt.Printf("処理をスキップします %v\n", subdir)
 			continue
 		}
 	}
 
-	fmt.Println("アドオンの配備が完了しました。")
+	fmt.Println("アドオンの配備が完了しました")
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/dabasan/addon-deployer
+
+go 1.15
+
+require (
+	github.com/otiai10/copy v1.4.2
+	github.com/urfave/cli/v2 v2.3.0
+)
